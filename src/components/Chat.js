9eb0d5e03f27d520e2ca94c60a1e0da1b4a98c5f
fancyaction <labcoatdisaster@gmail.com>
2018-05-10 18:30:21 -0500

    Add submit button

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import base, { firebaseApp } from "../base.js";
 
 export default class Chat extends Component {
+  commentRef = React.createRef();
+  
   state = {
     message: "",
     messages: []
@@ -27,30 +29,38 @@ export default class Chat extends Component {
 
   // Output messages as list items in chat
   renderChat = (message, i) => {
-
     return (
-      <li key={message.id}>
-        {message.name}: {message.comment}
+      <li className="msg" key={message.id}>
+        <span id="username">
+          <b>{message.name}</b>
+        </span>: <span id="comment" />
+        {message.comment}
       </li>
     );
   };
 
   // When user presses enter, submit message to Firebase
   handleMessage = ev => {
-    if (ev.keyCode === 13) {
       // NOTE: Add if statement to determine player later.
       const player1Name = this.props.player1Name;
       const player2Name = this.props.player2Name;
+      // console.log(player1Name, player2Name);
+      
       const message = {
         id: this.state.messages.length,
         name: player1Name,
-        comment: ev.target.value
+        comment: this.commentRef.value.value
       };
 
       firebaseApp
         .database()
         .ref("messages/" + message.id)
         .set(message);
+    };
+
+  pressEnter = ev => {
+    if (ev.keyCode === 13) {
+      this.handleMessage();
     }
   };
 
@@ -60,9 +70,11 @@ export default class Chat extends Component {
 
     return (
       <div>
-        <ul id='chatWindow'>{chatLog}</ul>
+        <ul id="chatWindow">{chatLog}</ul>
         <div id="nameRow">
-          <p>Name: <strong>{this.props.player1Name}</strong></p>
+          <p>
+            Name: <strong>{this.props.player1Name}</strong>
+          </p>
         </div>
         <div id="chatWindow" ref={this.chatWindowRef}>
           <ul id="messageList" ref={this.messageListRef} />
@@ -70,10 +82,14 @@ export default class Chat extends Component {
         <div>
           <input
             type="text"
-            id="message"
+            id="comment"
             placeholder="Enter your comment here"
-            onKeyDown={this.handleMessage}
+            onKeyDown={this.pressEnter}
+            ref={this.commentRef}
           />
+          <button onClick={this.handleMessage} id="submit">
+            Submit
+          </button>
         </div>
       </div>
     );
