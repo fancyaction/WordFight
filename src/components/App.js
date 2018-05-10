1bc6eb10cf6c9a5cca07ca24172814837ed134a2
fancyaction <labcoatdisaster@gmail.com>
2018-05-10 18:30:34 -0500

    Add player name prompt

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,41 @@ import Chat from "./Chat.js";
 import base from "../base.js";
 
 export default class App extends Component {
+  nameRef = React.createRef()
+  
   state = {
+    roomId: '',
     wordList: {},
-    player1Name: 'TestPlayer1',
-    player2Name: 'TestPlayer2',
+    player1Name: "",
+    player2Name: "",
     player1Score: 0,
     player2Score: 0
   };
 
+  enterName = ev => {
+    const name = this.nameRef.value.value;
+    this.setState({
+      player1Name: name
+    });
+
+    document.getElementById('playerNameField').remove();
+    document.getElementById('nameBut').remove();
+    }
+
   render() {
     return (
       <div>
+        <div>
+          <input type="text" id="playerNameField" ref={this.nameRef} placeholder='Enter username'/>
+          <button id='nameBut' onClick={this.enterName}>Enter Name</button>
+          
+        </div>
         {/* <Score />
         <Game /> */}
-        <Chat player1Name={this.state.player1Name} player2Name={this.state.player2Name} />
+        <Chat
+          player1Name={this.state.player1Name}
+          player2Name={this.state.player2Name}
+        />
       </div>
     );
   }
